07c6e2e39600e537fb63ddfd1e92c5f63b436b16
Akane Ishiguro <Ishiguro-Akane@s-giken.com>
2024-07-03 00:37:57 +0000

    7/3 KJBM010フォルダ作成移動

--- src/KJBM000/script.sh
+++ src/KJBM000/script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # 実行　（ 実行時詳細表示 + COPY区のデータファイルパスを指定 + 実行ファイル指定 ）
 # script.shの実行権限は、手入力chmod +x script.sh
-ls -al KJBM010.COB
-chmod +x KJBM010.COB
+ls -al KJBM010/KJBM010.COB
+chmod +x KJBM010/KJBM010.COB
 #ファイル確認処理
 if [ ! -f /workspaces/COBOL/copylib/KJCF010.COB ]; then
   echo "COPYの対象ファイルなし"
   exit 1
 fi
-cobc -x -v -I/workspaces/COBOL/copylib -o KJBM010 KJBM010.COB
+cobc -x -v -I/workspaces/COBOL/copylib -o KJBM010 KJBM010/KJBM010.COB
 # 権限追加
-chmod +x KJBM010
+chmod +x KJBM010/KJBM010
 # 実行
 ./KJBM010
 # OTF出力ファイの中身を確認
