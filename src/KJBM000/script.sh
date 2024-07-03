156d58f7cf6f04950d12db6d4c1346733018bab2
Akane Ishiguro <Ishiguro-Akane@s-giken.com>
2024-07-03 02:07:46 +0000

    7/3　スクリプトに環境変数

--- src/KJBM000/script.sh
+++ src/KJBM000/script.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
+export ITF="/workspaces/COBOL-1/data/KJBM010I.txt"
+export OTF="/workspaces/COBOL-1/src/KJBM000/KLBM010/KJBM010Icheck.txt"
 # 実行　（ 実行時詳細表示 + COPY区のデータファイルパスを指定 + 実行ファイル指定 ）
 # script.shの実行権限は、手入力chmod +x script.sh
-ls -al KJBM010/KJBM010.COB
-chmod +x KJBM010/KJBM010.COB
+ls -al /workspaces/COBOL-1/src/KJBM000/KLBM010/KJBM010.COB
+chmod +x /workspaces/COBOL-1/src/KJBM000/KLBM010/KJBM010.COB
 #ファイル確認処理
 if [ ! -f /workspaces/COBOL/copylib/KJCF010.COB ]; then
   echo "COPYの対象ファイルなし"
   exit 1
 fi
-cobc -x -v -I/workspaces/COBOL/copylib -o KJBM010 KJBM010/KJBM010.COB
+#コンパイル　　　　コピー項目の型　　　　　コンパイル作成場所　　　　　　　　　　　　　　　　　　コンパイル対象コードファイル　　　　　
+cobc -x -I/workspaces/COBOL/copylib -o /workspaces/COBOL-1/src/KJBM000/KLBM010/KJBM010 /workspaces/COBOL-1/src/KJBM000/KLBM010/KJBM010.COB
 # 権限追加
-chmod +x KJBM010/KJBM010
+chmod +x /workspaces/COBOL-1/src/KJBM000/KLBM010/KJBM010
 # 実行
 ./KJBM010
 # OTF出力ファイの中身を確認
-cat OTF
+cat "$OTF"
