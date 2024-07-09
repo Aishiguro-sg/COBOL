8def796c253a1351ac39ccac88cb37583720f941
Akane Ishiguro <Ishiguro-Akane@s-giken.com>
2024-07-09 04:15:47 +0000

    7/9 エラーレコード1と2に振り分け

--- src/KJBM050/run.script.sh
+++ src/KJBM050/run.script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # export ITF="../../data/KJBM010I.txt"
 export ITF="KJBM020Icheck.txt"
-export OTF1="KJBM050Ierror.txt"
-export OTF2="KJBM050Inoterror.txt"
+export OTF1="KJBM050Inoterror.txt"
+export OTF2="KJBM050Ierror.txt"
 # 実行
 ./KJBM050
