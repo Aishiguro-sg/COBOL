f270533c799144ec8d322c9502b61eaee60b047f
Akane Ishiguro <Ishiguro-Akane@s-giken.com>
2024-07-09 04:10:14 +0000

    7/9 出力先を2つに増やす

--- src/KJBM050/run.script.sh
+++ src/KJBM050/run.script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # export ITF="../../data/KJBM010I.txt"
 export ITF="KJBM020Icheck.txt"
-export OTF="KJBM050Icheck.txt"
+export OTF1="KJBM050Ierror.txt"
+export OTF2="KJBM050Inoterror.txt"
 # 実行
 ./KJBM050
