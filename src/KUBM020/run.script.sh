a4cc6ab853d1ce03ac5e6d2bcf9d41dd71ccad4a
Akane Ishiguro <Ishiguro-Akane@s-giken.com>
2024-07-16 00:50:24 +0000

    7/16 テストデータ作成

--- src/KUBM020/run.script.sh
+++ src/KUBM020/run.script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# export ITF="data.txt"
-export ITF="../../data/KUBM020I.txt"
-export OTF="OTFsam.txt"
+export ITF="KUBM020test.txt"
+# export ITF="../../data/KUBM020I.txt"
+export OTF="OTFsum.txt"
 # 実行
 ./KUBM020
