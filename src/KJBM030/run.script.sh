b25efd5c0189ce3cd6ba02d5e7f1f0e3af035e3e
Akane Ishiguro <ishiguro-akane@s-giken.com>
2024-07-18 06:26:07 +0000

    7/18 比較処理作成▶確認

--- src/KJBM030/run.script.sh
+++ src/KJBM030/run.script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export ITF="../../data/KJBM030IM.txt"
+export ITF="../../data/KJBM030I.txt"
+export IMF="../../data/KJBM030IM.txt"
 # export ITF="../../data/KUBM020I.txt"
 export OTF="OTFmatch.txt"
 # 実行
